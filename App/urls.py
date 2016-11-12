e99262fb379ec0440a94838c383e2dccd047fdcc
ArturAzarau <artur.azarau@gmail.com>
2016-11-12 16:39:27 +0300

    Функции,связанные с парсингом и базой данных вынесены в отдельный файл.  Добавлены индексы для полей event и time.

---
+++ App/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from App import views
+
+urlpatterns = [
+    url(r'^server/log/$', views.Find_in_databaseList.as_view()),
+    url(r'^server/log/(?P<pk>[$]+)/$', views.Find_in_databaseDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
