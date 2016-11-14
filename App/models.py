7d855e1129eb8ec24c8814fcb17bbaa002e68995
k5ii <k5ii@ya.ru>
2016-11-14 10:49:59 +0300

    REST updated

--- App/models.py
+++ App/models.py
@@ -5,3 +5,6 @@ class Find_in_database(models.Model):
     date_from = models.CharField(max_length=255)
     date_to = models.CharField(max_length=255)
     event = models.CharField(max_length=100)
+    interval = models.CharField(max_length=100)
+    number  = models.CharField(max_length=100)
+    offset  = models.CharField(max_length=100)
--- App/serializers.py
+++ App/serializers.py
@@ -6,4 +6,4 @@ from App.models import Find_in_database
 class Find_in_databaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Find_in_database
-        fields = ('date_from', 'date_to', 'event')
+        fields = ('date_from', 'date_to', 'event', 'interval', 'number', 'offset')
