0e1db8e6b5440ad4d8db69a91a4147908e2ad43d
ArturAzarau <artur.azarau@gmail.com>
2016-11-16 20:13:15 +0300

    Добавлена функция доставания значений из форм для ввода даты

---
+++ App/static/js/js_for_home_page.js
@@ -0,0 +1,5 @@
+function getdate() {
+    var first_date = document.getElementById('first_date').value;
+    var second_date = document.getElementById('second_date').value;
+    return [first_date, second_date];
+}
