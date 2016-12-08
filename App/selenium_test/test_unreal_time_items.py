9569fde35802971e8a707ca611decd6331e88d54
k5ii <k5ii@ya.ru>
2016-12-08 14:38:12 +0300

    some tests

---
+++ App/selenium_test/test_unreal_time_items.py
@@ -0,0 +1,25 @@
+from App.selenium_test.basic_selenium_test import BasicSeleniumTest
+
+
+TEST_URL = '/'
+TEST_BTN_ID = 'show-res'
+TEST_LST_CSS = '.prokrutka'
+TEST_FLTR_FROM_ID = 'first_date'
+TEST_FLTR_TO_ID = 'second_date'
+
+class TestUnrealTimeItems(BasicSeleniumTest):
+    def testUnrealTimeItems(self):
+        driver = self.getDriver()
+        driver.get(TEST_URL)
+        ffrom = driver.find_element_by_id(TEST_FLTR_FROM_ID)
+        fto = driver.find_element_by_id(TEST_FLTR_TO_ID)
+        ffrom.text = '1234-05-13 15:33:01.0'
+        fto.text = '1234-05-16 15:35:01.0'
+        button = driver.find_element_by_id(TEST_BTN_ID)
+        button.click()
+        listi = driver.find_element_by_css_selector(TEST_LST_CSS)
+        self.assertEqual("",listi)
+
+
+
+
