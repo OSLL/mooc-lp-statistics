ef6131628139397bd14cf77271b95880c62a8c30
k5ii <k5ii@ya.ru>
2016-12-19 10:42:48 +0300

    several events test

---
+++ App/selenium_test/test_several_events_search.py
@@ -0,0 +1,26 @@
+from App.selenium_test.basic_selenium_test import BasicSeleniumTest
+
+
+TEST_URL = 'http://127.0.0.1:8000/'
+TEST_BTN_ID = 'show-res'
+TEST_LST_CSS = '.prokrutka'
+TEST_CHECK_ID1 = 'first_event'
+
+
+class TestSeveralEventsSearch(BasicSeleniumTest):
+    def TestSeveralEventsSearch(self):
+        driver = self.getDriver()
+        driver.get(self.getUrl(TEST_URL))
+        checkbox1 = driver.find_element_by_id(TEST_CHECK_ID1)
+
+        button = driver.find_element_by_id(TEST_BTN_ID)
+        checkbox1.click()
+        eventText1 = 'pdaemon is already running'
+        # add other events if u need
+        # eventText2 = 'some other stuff'
+
+        button.click()
+        listi = driver.find_element_by_css_selector(TEST_LST_CSS)
+        self.assertIn(eventText1,listi)
+
+
