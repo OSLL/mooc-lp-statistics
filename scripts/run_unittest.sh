2ef5972747256620852ca2f87c28f9ec4454eac2
npatyur <n.y.patrakova@gmail.com>
2017-06-12 14:07:20 +0300

    Corrected exit code for run_unittest.sh

--- scripts/run_unittest.sh
+++ scripts/run_unittest.sh
@@ -4,7 +4,15 @@ echo
 echo "==================== UNITTESTS ====================="
 echo
 
+EXIST_ERROR_TEST=1
+NO_ERROR_TEST=0
+
 cd App/unittest
 PYTHONPATH=../../ python -m unittest discover -v 2>&1 | tee unittest.log
-exit_code="${PIPESTATUS[0]}"
-cat unittest.log
+
+if grep -c 'FAIL' unittest.log
+then
+    exit $EXIST_ERROR_TEST
+else
+    exit $NO_ERROR_TEST
+fi
