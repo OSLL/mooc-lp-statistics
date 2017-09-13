fdd73b7d87e078607412c91d47e2bae270ee2bfa
Natalya Patrakova <n.y.patrakova@gmail.com>
2017-09-13 16:22:35 +0300

    Update run_selenium_tests_in_firefox.sh

--- scripts/run_selenium_tests_in_firefox.sh
+++ scripts/run_selenium_tests_in_firefox.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CATALOG='mooc-lp-statistics'
+
 echo
 echo "==================== SELENIUMTESTS ====================="
 echo
 
-python ./manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword
+python /var/www/"$CATALOG"/manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword
 
 cd App/selenium_test
 PATH=./:$PATH python ./main.py http://mooc-lp-statistics/
 
-python ../../manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword --delete=yes
+python /var/www/"$CATALOG"/manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword --delete=yes
