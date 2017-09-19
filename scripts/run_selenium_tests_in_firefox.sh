319049cd9977169712d316a02b10b7fe1ec11783
npatyur <n.y.patrakova@gmail.com>
2017-09-19 17:42:25 +0300

    update local_deploy.sh create selenium-user

--- scripts/run_selenium_tests_in_firefox.sh
+++ scripts/run_selenium_tests_in_firefox.sh
@@ -6,9 +6,5 @@ echo
 echo "==================== SELENIUMTESTS ====================="
 echo
 
-python /var/www/"$CATALOG"/manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword
-
 cd App/selenium_test
 PATH=./:$PATH python ./main.py http://mooc-lp-statistics/
-
-python /var/www/"$CATALOG"/manage.py createadminuser --login=testSeleniumLogin --email=admin@testSeleniumLogin.ru --password=testSeleniumPassword --delete=yes
